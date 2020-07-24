bc3d1566966ffaf9fe0e2facd8eb5d35f4a4fda5
MuTian <mu_tian@human-horizons.com>
2020-07-24 13:17:19 +0800

    add support clickhouse

---
+++ presto-clickhouse/src/test/java/io/prestosql/plugin/clickhouse/ClickhouseJdbcConfigTest.java
@@ -0,0 +1,28 @@
+package io.prestosql.plugin.clickhouse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ClickhouseJdbcConfigTest {
+    public static void main(String[] args) throws Exception {
+
+        Connection connection = DriverManager.getConnection("jdbc:clickhouse://localhost:8123");
+
+        ResultSet resultSet = connection.getMetaData().getSchemas();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("TABLE_SCHEM") + " - "+ resultSet.getString("TABLE_CATALOG"));
+        }
+
+
+        System.out.println("===================================================");
+        Statement statement = connection.createStatement();
+        resultSet = statement.executeQuery("SHOW DATABASES");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+
+
+    }
+}
